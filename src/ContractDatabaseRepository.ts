1420899344099a55d0ec176278b6af995db0cbaa
Euller Cristian <eullercdr@gmail.com>
2023-11-04 15:15:31 -0300

    feat: create entities and clean code in use case

--- src/ContractDatabaseRepository.ts
+++ src/ContractDatabaseRepository.ts
@@ -1,18 +1,40 @@
+import Contract from "./Contract";
 import ContractRepository from "./ContractRepository";
 import DatabaseConnection from "./DatabaseConnection";
+import Payment from "./Payment";
 
 export default class ContractDatabaseRepository implements ContractRepository {
+  constructor(readonly connection: DatabaseConnection) {}
 
-  constructor(readonly connection: DatabaseConnection) {
-  }
-
-  async list(): Promise<any> {
-    const contracts = await this.connection.query("SELECT * FROM mba.contract", []);
-    for (const contract of contracts) {
-      contract.payments = await this.connection.query("SELECT * FROM mba.payment WHERE id_contract = $1",
-        [contract.id_contract]);
+  async list(): Promise<Contract[]> {
+    const contracts: Contract[] = [];
+    const contractsData = await this.connection.query(
+      "SELECT * FROM mba.contract",
+      []
+    );
+    for (const contractData of contractsData) {
+      const contract = new Contract(
+        contractData.id_contract,
+        contractData.description,
+        contractData.amount,
+        contractData.periods,
+        contractData.date
+      );
+      const paymentsData = await this.connection.query(
+        "SELECT * FROM mba.payment WHERE id_contract = $1",
+        [contract.idContract]
+      );
+      for (const paymentData of paymentsData) {
+        let payment = new Payment(
+          paymentData.id_payment,
+          paymentData.date,
+          parseFloat(paymentData.amount)
+        );
+        contract.addPayment(payment);
+      }
+      contracts.push(contract);
     }
     await this.connection.close();
-    return contracts
+    return contracts;
   }
 }
--- src/GenerateInvoices.ts
+++ src/GenerateInvoices.ts
@@ -1,41 +1,24 @@
 import moment from "moment";
 import ContractRepository from "./ContractRepository";
 export default class GenerateInvoices {
-
-  constructor(readonly contractRepository: ContractRepository) {
-  }
+  constructor(readonly contractRepository: ContractRepository) {}
 
   async execute(input: Input): Promise<Output[]> {
     const output: Output[] = [];
     const contracts = await this.contractRepository.list();
     for (const contract of contracts) {
-      if (input.type === 'cash') {
-        for (const payment of contract.payments) {
-          if (payment.date.getMonth() + 1 !== input.month || payment.date.getFullYear() !== input.year) {
-            continue;
-          }
-          output.push({
-            date: moment(payment.date).format("YYYY-MM-DD"),
-            amount: parseFloat(payment.amount)
-          });
-        }
-      }
-      if (input.type === 'accrual') {
-        let period = 0;
-        while (period <= contract.periods) {
-          const date = moment(contract.date).add(period++, 'month').toDate();
-          if (date.getMonth() + 1 !== input.month || date.getFullYear() !== input.year) {
-            continue;
-          }
-          const amount = parseFloat(contract.amount) / contract.periods;
-          output.push({
-            date: moment(date).format("YYYY-MM-DD"),
-            amount: amount
-          });
-        }
+      const invoices = contract.generateInvoices(
+        input.month,
+        input.year,
+        input.type
+      );
+      for (const invoice of invoices) {
+        output.push({
+          date: moment(invoice.date).format("YYYY-MM-DD"),
+          amount: invoice.amount,
+        });
       }
     }
-
     return output;
   }
 }
@@ -44,9 +27,9 @@ type Input = {
   month: number;
   year: number;
   type: string;
-}
+};
 
 type Output = {
   date: string;
   amount: number;
-}
+};
